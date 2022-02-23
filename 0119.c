fd5fd8a2ee768dc2490e3a7921559a52d1176943
chuxuezhemaxiao <98212418+chuxuezhemaxiao@users.noreply.github.com>
2022-02-23 14:05:38 +0800

    Add files via upload

---
+++ 0119.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+int main()
+{int m,n;
+scanf("%d  ",&m);
+scanf("%d",&n);
+int a;
+int x=a+a-1;
+while (m<x<n)
+{printf("%d",x);
+};
+return 0;
+
+
+
+
+}
